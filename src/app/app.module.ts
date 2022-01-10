6a918ab4abbfca729b593b7751dda26023a83041
kesvesna <kesvesna@rambler.ru>
2022-01-10 19:30:16 +0300

    Installed, created modules, components, sass. Import style files to global sass file.

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from "./modules/material/material.module";
+import { LayoutComponent } from './components/layout/layout/layout.component';
+import { NavigationComponent } from './components/layout/navigation/navigation.component';
+import { PreloaderComponent } from './components/layout/preloader/preloader.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LayoutComponent,
+    NavigationComponent,
+    PreloaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
